a116b7e122273862db2b10043005b03fc7e4ecd7
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-09-22 22:58:39 -0400

    Update Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
-//
+const Intern = require("../lib/Intern");
+
+test("Can create school.", () => {
+    const testSchool = "School Name";
+    const employeeRecord = new Intern("Gordon", 2, "gordon@gmail.com", testSchool);
+    expect(employeeRecord.school).toBe(testSchool);
+});
+
+test("Testing officeNumber will return office number.", () => {
+    const testSchool = "School Name";
+    const employeeRecord = new Intern("Gordon", 2, "gordon@gmail.com", testSchool);
+    expect(employeeRecord.getSchool()).toBe(testSchool);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Intern";
+    const employeeRecord = new Intern("Gordon", 2, "gordon@gmail.com", "School Name");
+    expect(employeeRecord.getRole()).toBe(returnValue);
+});
