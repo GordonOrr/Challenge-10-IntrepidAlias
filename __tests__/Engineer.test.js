be3a8e3e3e0fb26b2a79b9c8538de9872c8a20ff
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-09-22 22:58:58 -0400

    Update Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
-//
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+    const testGithub = "GordonOrr";
+    const employeeRecord = new Engineer("Gordon", 2, "gordon@gmail.com", testGithub);
+    expect(employeeRecord.github).toBe(testGithub);
+});
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "GordonOrr";
+    const employeeRecord = new Engineer("Gordon", 2, "gordon@gmail.com", testGithub);
+    expect(employeeRecord.getGithub()).toBe(testGithub);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeRecord = new Engineer("Gordon", 2, "gordon@gmail.com", "GordonOrr");
+    expect(employeeRecord.getRole()).toBe(returnValue);
+});
