a9af7651afd8fc8cc5a6687017e43ec7d5d514f2
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-09-22 22:59:25 -0400

    Update Manager.test.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,21 @@
-//
+// Include Manager.js component
+const Manager = require("../lib/Manager");
+
+// Unit Tests
+test("Can create an office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeRecord = new Manager("Gordon", 2, "gordon@gmail.com", testOfficeNumber);
+    expect(employeeRecord.officeNumber).toBe(testOfficeNumber);
+});
+
+test("Testing officeNumber will return office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeRecord = new Manager("Gordon", 2, "gordon@gmail.com", testOfficeNumber);
+    expect(employeeRecord.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Manager";
+    const employeeRecord = new Manager("Gordon", 2, "gordon@gmail.com", 2);
+    expect(employeeRecord.getRole()).toBe(returnValue);
+});
