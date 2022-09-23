b612247b0d782b6224709ef0c1458560b03eb74c
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-09-22 23:14:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-//
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
